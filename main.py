bcfdbda877e479e88dd1727e768d0162c251ed86
Rushikesh Kulkarni <rushiknight@gmail.com>
2025-09-15 10:55:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -244,5 +244,13 @@ elif app_mode == "Disease Recognition":
                         )
                     else:
                         st.warning("💡 Cure information not available for this disease.")
+                        hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
